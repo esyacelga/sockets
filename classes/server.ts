0e75df7893c6a2e93f3058f24843dbf828ea535e
santiago.yacelga <esyacelga@hotmail.com>
2019-03-11 17:26:57 -0500

    Litener SOCKET

--- classes/server.ts
+++ classes/server.ts
@@ -1,19 +1,37 @@
 import express from 'express';
 import {SERVER_PORT} from "../global/enviroment";
+import socketIO from 'socket.io';
+import http from 'http';
 
 export default class Server {
     public app: express.Application;
     public port: number;
 
 
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
     constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+        this.escucharSockets();
 
     }
 
+
+    private escucharSockets() {
+        console.log('Escuchando concexiones--sockets');
+        this.io.on('connection', cliente => {
+            console.log('Cliente concectado');
+        });
+    }
+
     start(callback: Function) {
-        this.app.listen(this.port, callback);
+        //this.app.listen(this.port, callback);
+        // @ts-ignore
+        this.httpServer.listen(this.port, callback)
     }
 
 
